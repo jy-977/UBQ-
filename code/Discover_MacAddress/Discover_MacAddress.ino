63941f8e6c53dc346d48235ba9ece08428ba4ec0
Danillo Lange <dls5@alumnes.udl.cat>
2020-10-20 12:06:52 +0200

    Changes in documentation

---
+++ code/Discover_MacAddress/Discover_MacAddress.ino
@@ -0,0 +1,16 @@
+// Complete Instructions to Get and Change ESP MAC Address: https://RandomNerdTutorials.com/get-change-esp32-esp8266-mac-address-arduino/
+
+
+#include <ESP8266WiFi.h>
+#endif
+
+void setup(){
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("ESP Board MAC Address:  ");
+  
+}
+ 
+void loop(){
+  Serial.println(WiFi.macAddress());
+}
